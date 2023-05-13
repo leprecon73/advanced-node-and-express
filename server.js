e2557aabac9962f5888c2c3c7003f55ef7233c2b
leprecon73 <leprecon73@gmail.com>
2023-05-13 19:40:11 +0400

    06

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 app.route('/').get((req, res) => {
-  res.render(index, './views/pug')
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
