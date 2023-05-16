bf7dc5c18cf14afb8f7efa733ab48e978c220101
leprecon73 <leprecon73@gmail.com>
2023-05-16 10:18:49 +0400

    2.5.9

--- server.js
+++ server.js
@@ -35,32 +35,27 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-myDB(async client => {
+myDB(async (client) => {
   const myDataBase = await client.db('mongo-db').collection('collection-users');
-
-  // Be sure to change the title
-  app.route('/').get((req, res) => {
-    // Change the response to render the Pug template
-    res.render('index', {
-      title: 'Connected to Database',
-      message: 'Please login'
-    });
+  
+  app.get("/", (req, res) => {
+    res.render(process.cwd() + '/views/pug/index', 
+    {title: 'Connected to DB', message: 'Please login'});
   });
 
-  // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
     done(null, user._id);
   });
   
   passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
+    myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+      done(null,doc);
     });
   });
 
-  // Be sure to add this...
-}).catch(e => {
-  app.route('/').get((req, res) => {
-    res.render('index', { title: e, message: 'Unable to connect to database' });
+}).catch((e) => {
+ app.get("/", (req, res) => {
+    res.render(process.cwd() + '/views/pug/index', 
+    {title: e, message: 'Unable to login'});
   });
 });
